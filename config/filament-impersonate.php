102f7185dc149dbd9e77c9af3028f474f5546f95
Ian Tasker <ianwtasker@outlook.com>
2021-12-09 19:57:26 +0000

    remove redundant config setting

--- config/filament-impersonate.php
+++ config/filament-impersonate.php
@@ -3,9 +3,6 @@ return [
     // This is the guard used when logging in as the impersonated user.
     'default_guard' => env('FILAMENT_IMPERSONATE_GUARD', 'web'),
 
-    // After impersonating this is where we'll redirect you to.
-    'redirect_to' => env('FILAMENT_IMPERSONATE_REDIRECT', '/'),
-
     // We wire up a route for the "leave" button. You can change the middleware stack here if needed.
     'leave_middleware' => env('FILAMENT_IMPERSONATE_LEAVE_MIDDLEWARE', 'web'),
 
