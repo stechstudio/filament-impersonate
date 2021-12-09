7e3ea3bb45bc6e634417c8b1f5194a133d4b3e82
Ian Tasker <ianwtasker@outlook.com>
2021-12-09 21:27:28 +0000

    Revert missing config value

--- config/filament-impersonate.php
+++ config/filament-impersonate.php
@@ -2,6 +2,9 @@
 return [
     // This is the guard used when logging in as the impersonated user.
     'guard' => env('FILAMENT_IMPERSONATE_GUARD', 'web'),
+    
+    // After impersonating this is where we'll redirect you to.
+    'redirect_to' => env('FILAMENT_IMPERSONATE_REDIRECT', '/'),
 
     // We wire up a route for the "leave" button. You can change the middleware stack here if needed.
     'leave_middleware' => env('FILAMENT_IMPERSONATE_LEAVE_MIDDLEWARE', 'web'),
