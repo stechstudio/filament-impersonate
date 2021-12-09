de3e563c5fd29d4ed6cd05ee4599f6391f0523ac
Ian Tasker <ianwtasker@outlook.com>
2021-12-09 21:24:23 +0000

    updated config key

--- config/filament-impersonate.php
+++ config/filament-impersonate.php
@@ -1,7 +1,7 @@
 <?php
 return [
     // This is the guard used when logging in as the impersonated user.
-    'default_guard' => env('FILAMENT_IMPERSONATE_GUARD', 'web'),
+    'guard' => env('FILAMENT_IMPERSONATE_GUARD', 'web'),
 
     // We wire up a route for the "leave" button. You can change the middleware stack here if needed.
     'leave_middleware' => env('FILAMENT_IMPERSONATE_LEAVE_MIDDLEWARE', 'web'),
