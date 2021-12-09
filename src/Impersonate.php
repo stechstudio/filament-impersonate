4c0154df5b4b355cd9af3fb8debd1f3e9d8d61c1
Ian Tasker <ianwtasker@outlook.com>
2021-12-09 16:40:33 +0000

    updated impersonate class

--- src/Impersonate.php
+++ src/Impersonate.php
@@ -2,51 +2,16 @@
 
 namespace STS\FilamentImpersonate;
 
-use Filament\Tables\RecordActions\Action;
-use Filament\Tables\RecordActions\Concerns\CanCallAction;
+use Filament\Tables\Actions\Action;
 
 class Impersonate extends Action
 {
-    use CanCallAction;
-
     protected $view = 'impersonate::icon';
-
-    protected $guard;
-
-    protected $redirect;
 
     public static function make($name = 'impersonate')
     {
         return (new static($name))
             ->when(fn ($record) => static::allowed(auth()->user(), $record));
-    }
-
-    public function guard($guard)
-    {
-        $this->configure(function () use ($guard) {
-            $this->guard = $guard;
-        });
-
-        return $this;
-    }
-
-    public function getGuard()
-    {
-        return $this->guard;
-    }
-
-    public function redirect($redirect)
-    {
-        $this->configure(function () use ($redirect) {
-            $this->redirect = $redirect;
-        });
-
-        return $this;
-    }
-
-    public function getRedirect()
-    {
-        return $this->redirect;
     }
 
     public static function allowed($current, $target)
