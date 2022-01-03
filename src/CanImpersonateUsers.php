ecac587c1f6e7d88c0e5b4448298193ef0a0d374
Joseph Szobody <joseph@stechstudio.com>
2022-01-03 15:45:09 -0500

    forget the password_hash when switching users

--- src/CanImpersonateUsers.php
+++ src/CanImpersonateUsers.php
@@ -19,6 +19,7 @@ trait CanImpersonateUsers
             auth()->user(), $target, config('filament-impersonate.guard')
         );
 
+        session()->forget('password_hash_web');
         session()->put('impersonate.back_to', request('fingerprint.path'));
 
         return redirect(isset($this->url) ? $this->url : config('filament-impersonate.redirect_to'));
