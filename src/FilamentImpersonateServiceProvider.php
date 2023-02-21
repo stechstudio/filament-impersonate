e628df3c7c55eff1bf472b587ecde3ef48be4f58
Joseph Szobody <joseph@stechstudio.com>
2023-02-20 21:07:43 -0500

    rework the service provider using the plugin/package hooks and auto discovery

--- src/FilamentImpersonateServiceProvider.php
+++ src/FilamentImpersonateServiceProvider.php
@@ -6,6 +6,7 @@ use Filament\PluginServiceProvider;
 use Illuminate\Support\Facades\Event;
 use Lab404\Impersonate\Events\LeaveImpersonation;
 use Lab404\Impersonate\Events\TakeImpersonation;
+use Spatie\LaravelPackageTools\Package;
 use STS\FilamentImpersonate\Middleware\ImpersonationBanner;
 use STS\FilamentImpersonate\Tables\Actions\Impersonate;
 
@@ -13,7 +14,7 @@ class FilamentImpersonateServiceProvider extends PluginServiceProvider
 {
     public static string $name = 'filament-impersonate';
 
-    public function register()
+    public function registeringPackage()
     {
         $this->app['config']->push('filament.middleware.base', ImpersonationBanner::class);
 
@@ -21,15 +22,15 @@ class FilamentImpersonateServiceProvider extends PluginServiceProvider
         Event::listen(LeaveImpersonation::class, fn() => $this->clearAuthHashes());
     }
 
-    public function boot()
+    public function packageConfiguring(Package $package): void
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/filament-impersonate.php', 'filament-impersonate');
+        $package->hasRoute('web');
+    }
 
+    public function bootingPackage()
+    {
+        // For backwards compatibility we're going to load our views into the namespace we used to use as well.
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'impersonate');
-
-        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
-
-        $this->loadTranslationsFrom(__DIR__.'/../lang', 'filament-impersonate');
 
         // Alias our table action for backwards compatibility.
         // STS\FilamentImpersonate\Impersonate is where that class used to exist, and I don't
