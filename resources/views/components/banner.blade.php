7b628915d5f04d34f7e7ba74df29a451bc70e2e4
Lars Beerepoot <Charathan@users.noreply.github.com>
2024-01-29 16:51:53 +0100

    set banner below notifications (#62)

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -47,7 +47,7 @@ $flipped = $default === 'dark' ? 'light' : 'dark';
         background-color: var(--impersonate-{{ $default }}-bg-color);
         color: var(--impersonate-{{ $default }}-text-color);
         border-{{ $borderPosition }}: 1px solid var(--impersonate-{{ $default }}-border-color);
-        z-index: 1000;
+        z-index: 45;
     }
 
     @if($style === 'auto')
