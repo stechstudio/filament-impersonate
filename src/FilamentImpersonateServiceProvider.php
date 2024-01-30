700ab5466f8e2a546d622dfe3ca83faaf88d9bfb
Alex Dills <alex@stechstudio.com>
2024-01-30 09:58:18 -0500

    Default banner hook config value (#68)
    * Initializing Default_banner_hook_config_value for empty Pull Request
    
    * Add default value to config() call

--- src/FilamentImpersonateServiceProvider.php
+++ src/FilamentImpersonateServiceProvider.php
@@ -38,7 +38,7 @@ class FilamentImpersonateServiceProvider extends PackageServiceProvider
     public function bootingPackage(): void
     {
         FilamentView::registerRenderHook(
-            config('filament-impersonate.banner.render_hook'),
+            config('filament-impersonate.banner.render_hook', 'panels::body.start'),
             static fn (): string => Blade::render("<x-filament-impersonate::banner/>")
         );
 
