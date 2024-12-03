8a3ac8716f16de373fcd2ec565bcd8e242665443
Xavi Cabot <xavi@unancor.com>
2024-12-03 18:06:07 +0100

    Add Spanish translation

--- resources/lang/es/action.php
+++ resources/lang/es/action.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'label' => 'تقليد',
+    'label' => 'Personificar',
 ];
--- resources/lang/es/banner.php
+++ resources/lang/es/banner.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'impersonating' => 'المستخدم المقلّد',
-    'leave' => 'مغادرة',
+    'impersonating' => 'Haciéndose pasar por usuario',
+    'leave' => 'Salir',
 ];
